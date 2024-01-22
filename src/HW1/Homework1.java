226afdc7c7ec1336825f4a515e6f3d7658b3ed38
erinchc <erinchen25@gmail.com>
2024-01-22 20:44:43 +0800

    Homework1_java practice

--- src/HW1/Homework1.java
+++ src/HW1/Homework1.java
@@ -3,7 +3,6 @@ package HW1;
 public class Homework1 {
 	public static void main(String[] args) {
 		// 計算12和6的和與積
-		System.out.println("1. ");
 
 		int num1 = 12, num2 = 6;
 		System.out.println("12, 6的和= " + (num1 + num2));
@@ -11,14 +10,12 @@ public class Homework1 {
 		System.out.println("------------------------");
 
 		// 計算200顆蛋共是幾打幾顆?
-		System.out.println("2.");
 
 		int totalEgg = 200, perDozen = 12;
 		System.out.println("200顆蛋 = " + totalEgg / perDozen + "打" + totalEgg % perDozen + "顆");
 		System.out.println("------------------------");
 
 		// 計算256559秒為多少天、多少小時、多少分與多少秒
-		System.out.println("3.");
 
 		int totalSeconds = 256559;
 		int days = totalSeconds / 86400;
@@ -29,7 +26,6 @@ public class Homework1 {
 		System.out.println("------------------------");
 
 		// 定義一個常數為3.1415(圓周率),計算半徑為5的圓面積與圓周長
-		System.out.println("4.");
 
 		final double PI = 3.1415;
 		int radius = 5;
@@ -40,8 +36,6 @@ public class Homework1 {
 		System.out.println("------------------------");
 
 		// 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本金加利息共有多少錢
-		// (用複利計算,公式請自行google)
-		System.out.println("5.");
 
 		int principal = 1500000, years = 10;
 		double rate = 0.02;
@@ -53,8 +47,6 @@ public class Homework1 {
 		// 5 + 5
 		// 5 + '5'
 		// 5 + "5"
-
-		System.out.println("6.");
 
 		System.out.println(5 + 5); // 兩個整數相加，結果為10。
 		System.out.println(5 + '5'); // '5'是字元char，會被轉換成unicode 53，結果為5+53=58。
