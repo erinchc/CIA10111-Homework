b55e8cd398eff735546dc230daa6d73b8fc0d894
erinchc <erinchen25@gmail.com>
2024-02-16 22:57:05 +0800

    Homework7 File, I/O

--- src/HW7/Homework7_3.java
+++ src/HW7/Homework7_3.java
@@ -6,36 +6,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Homework7_3 {
-	
+
 	// 請從無到有試著完成一個方法名為copyFile,這個方法有兩個參數。
-    // 呼叫此方法時,第一個參數所代表的檔案會複製到第二個參數代表的檔案
+	// 呼叫此方法時,第一個參數所代表的檔案會複製到第二個參數代表的檔案
 
-    public static void copyFile(File input, File output) throws IOException {
+	public static void copyFile(File input, File output) throws IOException {
 
-        FileReader in = new FileReader(input);
-        FileWriter out = new FileWriter(output);
+		FileReader in = new FileReader(input);
+		FileWriter out = new FileWriter(output);
 
-        int i;
-        while ((i = in.read()) != -1) {
-            out.write(i);
-            System.out.print((char) i);
-            System.out.flush();
-        }
+		int i;
+		while ((i = in.read()) != -1) {
+			out.write(i);
+			System.out.print((char) i);
+			System.out.flush();
+		}
 
-        in.close();
-        out.close();
-    }
+		in.close();
+		out.close();
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        try {
-            File input = new File("C:\\course\\src\\Hello.txt");
-            File output = new File("C:\\course\\src\\Hello2.txt");
-            copyFile(input, output);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+		try {
+			File input = new File("C:\\course\\src\\Hello.txt");
+			File output = new File("C:\\course\\src\\Hello2.txt");
+			copyFile(input, output);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 
-    }
+	}
 
 }
